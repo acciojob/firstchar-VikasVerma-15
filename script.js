35ae042737543cf3c88e9ca1973ac2bff9be632f
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-24 13:54:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function firstChar(text) {
-  // your code here
- for (let i = 0; i < text.length; i++) {
+  if (!text) return '';
+
+  for (let i = 0; i < text.length; i++) {
     if (text[i] !== ' ') {
       return text[i];
     }
   }
   return '';
 }
+
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
